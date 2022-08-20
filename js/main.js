eb73651f1f154b900ef07d289f7dbe6864391c7c
Dmitriy An <dmitrii.7774@gmail.com>
2022-08-20 21:26:44 +1100

    feat: Added a ban on scrolling when the menu is open

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ const burger = document.querySelector('.nav__burger'),
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-open')
-    
+    nav.classList.contains('nav-open') ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'scroll'
+
 })
